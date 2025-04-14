5bc37cb2d87cbe0f06c4b0727e3819fed31e8f39
Daryl Lee <daryl@daryllee.com>
2025-04-14 14:45:38 -0600

    Sample demo added

--- demos/Sample.py
+++ demos/Sample.py
@@ -23,20 +23,9 @@ import sys
 import os
 import numpy as np
 
-# [Moore] pg 94ff
-class LabelInput(tk.Frame):
-    # Note that the space after "together" is important
-    """A label and input combined together"""
-    def __init__(self, parent, label, inp_cls, inp_args, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-        self.label = tk.Label(self, text=label, anchor='w')
-        self.input = inp_cls(self, **inp_args)
-        self.columnconfigure(1, weight=1)
-        self.label.grid(sticky=tk.E + tk.W)
-        self.input.grid(row=0, column=1, sticky=tk.E + tk.W)
-
 class Application(tk.Tk):
     def __init__(self, *args, **kwargs):
+        '''Set up GUI interface'''
         super().__init__(*args, **kwargs)
         self.started = False
         self.img = None
@@ -55,17 +44,19 @@ class Application(tk.Tk):
         self.samples = tk.StringVar(value = '256x256')
         self.samples.trace_add("write", self.updateSamples)
         
-       # Controls
+        # Controls
         tk.Spinbox(self,
                    from_=1.0, to=2.0, increment=0.1,
                    textvariable=self.zf).grid(row = 0,  column = 0, sticky = 'nwes')
         tk.Label(self, textvariable = 
                               self.lbl1_txt).grid(row = 0, column = 1)
-        value_strings = ['256x256', '128x128', '64x64', '32x32']
+
+        value_strings = ['32x32', '64x64', '128x128', '256x256']
         tk.Spinbox(self,
                    values = value_strings,
                    textvariable = 
                       self.samples).grid(row = 1, column = 0, sticky = 'nwes')
+        self.samples.set('256x256')
         tk.Label(self, textvariable=self.lbl2_txt).grid(row = 1, column = 1)
         
         tk.Button(self, text = "Read Image", 
@@ -80,6 +71,8 @@ class Application(tk.Tk):
       
         
     def loadImage(self):
+        '''Load imafe using system file selector starting at SIVA_IMAGES_PATH,
+           if it is defined, current directory if it is not defined.'''
         images_at = os.getenv("SIVA_IMAGES_PATH", default=".")
         file_path = filedialog.askopenfilename(
                            initialdir=images_at, 
@@ -89,7 +82,7 @@ class Application(tk.Tk):
                                ("All files", "*.*")))
         self.img_orig = cv2.imread(file_path)
         if self.img_orig is not None:
-            self.img_proc = self.img_orig.clone()
+            self.img_proc = self.img_orig.copy()
  
     
     def startProc(self):
@@ -102,16 +95,16 @@ class Application(tk.Tk):
     def stopProc(self):
         if self.started:
             cv2.destroyAllWindows()
-            sys.exit(0) 
+        sys.exit(0) 
 
     def updateSize(self, name, index, mode):
+        '''Resize original image by requested scale factor'''
         if not self.started:
             return
         
         zoom_factor =self.zf.get()
         h,w = self.img_orig.shape[:2]
         new_size = (int(zoom_factor * h), int(zoom_factor * w))
-        print(f'Zoom Factor = {zoom_factor}; new size = {new_size}\n')
         self.img = cv2.resize( self.img, new_size )
      
         self.img_proc = cv2.resize( self.img_proc, new_size )
@@ -120,30 +113,22 @@ class Application(tk.Tk):
         self.img = cv2.resize( self.img, new_size )
         cv2.imshow('Original', self.img)
 
-    def resample(img_orig, new_dims):
-      
-        times =
-        old_shape = img_orig.shape  
-        old_rows = old_shape[0]
-        new_rows = old_shape[0]
-        new_rows = new_dims[0]
-        new_cols = new_dims[1]
-
-        mask = np.ones(new_dims[0], new_dims[1]) * (1 / (new_dims[0] * new_dims[1]))
-
-
 
     def updateSamples(self, name, index, mode):
+        '''Resize image to requested dimensions'''
         if not self.started:
             return
         
-        str = self.samples.get()
-        # parse the substring AFTER the 'x'
-        ndx = 1 + str.find('x')
-        samples = int(str[ndx:])
-        self.img_proc = self.img_orig.clone()
-        print(f'{samples}x{samples}')
-        self.img_proc = resample(self.img_orig, (samples, samples))
+        sstr = self.samples.get()
+        # parse the substrings before and after the 'x'
+        ndx = sstr.find('x')
+        new_height = int(sstr[:ndx])
+        new_width = int(sstr[ndx + 1:])
+        
+        self.img_proc = cv2.resize(self.img_orig, 
+                                   (new_width, new_height), 
+                                   interpolation=cv2.INTER_AREA)
+
         cv2.imshow('Processed',self.img_proc)
     
     
